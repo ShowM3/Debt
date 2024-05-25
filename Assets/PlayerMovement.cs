583cb9b9a651bf5e2298b4364fec323b22c009b2
TheKingJasper09 <nathanc1701@gmail.com>
2024-05-25 14:29:36 -0400

    one sided Dash and double jump
    Dashes only to the right
    Double jump and jump variations are complete.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,39 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine; 
-
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float jump;
-
     private float Move;
-
     public Rigidbody2D rb;
-
     public bool isJumping;
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 7f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+    private float jumpingPower = 8f;
+    private bool doubleJump;
+
+    [SerializeField] private TrailRenderer tr;
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move = Input.GetAxis("Horizontal");
-
-        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump") && isJumping == false)
+        if (isDashing)
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            return;
         }
 
-    }
+        if (IsGrounded() && !Input.GetButton("Jump"))
+        {
+            doubleJump = false;
+        }
 
+        Move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded() || doubleJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+                doubleJump = !doubleJump;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -41,7 +66,6 @@ public class PlayerMovement : MonoBehaviour
         {
             isJumping = false;
         }
-
     }
 
     private void OnCollisionExit2D(Collision2D other)
@@ -50,5 +74,29 @@ public class PlayerMovement : MonoBehaviour
         {
             isJumping = true;
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+
+    private bool IsGrounded()
+    {
+        // Implement your own logic to check if the player is grounded
+        // For example, you can use a raycast or a collision check
+        // Return true if the player is grounded, false otherwise
+        return true; // Replace this with your own grounded check logic
     }
 }
