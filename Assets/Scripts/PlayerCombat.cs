1d192355a7c8209c9bffdb4b4cda3253f8f0a3de
ShowM3 <rjymon@gmail.com>
2024-05-27 16:31:36 -0400

    Started PlayerCombat and added a scripts folder
    Scripts folder now holds all code for organization

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Attack(); 
+        }
+    }
+
+    void Attack()
+    {
+        //Play an attack animation
+        //Detect enemies in range of attack
+        //Damage the enemies
+
+
+    }
+
+}
